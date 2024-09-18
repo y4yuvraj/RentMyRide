3b4a5d56177e8be5af45e04d1ecc5ca83dddc3ab
yuvraj <46413950+y4yuvraj@users.noreply.github.com>
2024-09-18 15:14:14 +0530

    added basic project structure

---
+++ RentMyRide/src/main/java/in/yuvsingh/RentMyRide/RentMyRideApplication.java
@@ -0,0 +1,13 @@
+package in.yuvsingh.RentMyRide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentMyRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentMyRideApplication.class, args);
+	}
+
+}
---
+++ RentMyRide/src/test/java/in/yuvsingh/RentMyRide/RentMyRideApplicationTests.java
@@ -0,0 +1,13 @@
+package in.yuvsingh.RentMyRide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentMyRideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
